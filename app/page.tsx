db53cd3841010fee99c3afec0338bf1030a3a643
Roméo REL <romeorel99@gmail.com>
2025-10-24 11:44:05 +0300

    removing duplicate title

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,7 @@ import ContactPage from "./contact/page";
 export default function Home() {
   return (
     <>
-      <h1 className="bg-red-500">
-        Hello, My name is Roméo Rel - Next.js Developer
-      </h1>
+      
       <p>
         I’m Romeo Rel, a freelance Next.js developer specializing in
         high-performance web applications using React and Tailwind CSS.
